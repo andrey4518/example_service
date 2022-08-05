dc7a905484ad33c00f9b2894e90f42b1254b72f1
Andrey Sinyaev <andrey4518@gmail.com>
2022-08-05 12:20:50 +0300

    add rating generation activity

--- activity_generator_service/app/local_db_queries.py
+++ activity_generator_service/app/local_db_queries.py
@@ -1,8 +1,9 @@
 import asyncio
 from typing import List, Optional
+import sqlalchemy as sa
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy import func, select
-from sqlalchemy.orm import sessionmaker, aliased
+from sqlalchemy.orm import sessionmaker
 import local_db_dto
 from async_lru import alru_cache
 from log import logger
@@ -35,7 +36,7 @@ async def get_random_user_id():
 
 
 async def set_user_service_id(user_id: int, service_id: Optional[int]):
-    engine = create_local_db_engine()
+    engine = await create_local_db_engine()
     async_session = sessionmaker(
         engine, expire_on_commit=False, class_=AsyncSession
     )
@@ -47,6 +48,18 @@ async def set_user_service_id(user_id: int, service_id: Optional[int]):
         user.service_id = service_id
         user.exported = service_id is not None
         await session.commit()
+
+
+async def get_exported_users():
+    engine = await create_local_db_engine()
+    async_session = sessionmaker(
+        engine, expire_on_commit=False, class_=AsyncSession
+    )
+
+    async with async_session() as session:
+        stmt = select(local_db_dto.User).where(local_db_dto.User.exported)
+        result = await session.execute(stmt)
+        return [row.User for row in result]
 
 
 async def get_random_movie() -> local_db_dto.Movie:
@@ -104,7 +117,6 @@ async def set_movie_exported(movie_id, service_id):
             .join(local_db_dto.Movie.links)\
             .join(local_db_dto.Movie.movie_genre)\
             .where(local_db_dto.Movie.id == movie_id)
-        logger.info(stmt)
         for r in await session.execute(stmt):
             r.Movie.service_id = service_id
             r.Movie.exported = service_id is not None
@@ -123,3 +135,72 @@ async def get_exported_movies():
         stmt = select(local_db_dto.Movie).where(local_db_dto.Movie.exported)
         result = await session.execute(stmt)
         return [row.Movie for row in result]
+
+
+async def get_rating_ready_to_export():
+    engine = await create_local_db_engine()
+    async_session = sessionmaker(
+        engine, expire_on_commit=False, class_=AsyncSession
+    )
+
+    async with async_session() as session:
+        stmt = select(local_db_dto.Rating)\
+            .join(local_db_dto.Rating.movie)\
+            .join(local_db_dto.Rating.user)\
+            .where(sa.and_(
+                local_db_dto.Rating.exported == False,
+                local_db_dto.Movie.exported,
+                local_db_dto.User.exported
+            )).order_by(func.random()).limit(1)
+        result = await session.execute(stmt)
+        rating = result.fetchone()
+        if rating is None:
+            return None
+        else:
+            rating = rating[0]
+        return rating
+
+
+async def get_all_ratings_ready_to_export():
+    engine = await create_local_db_engine()
+    async_session = sessionmaker(
+        engine, expire_on_commit=False, class_=AsyncSession
+    )
+
+    async with async_session() as session:
+        stmt = select(local_db_dto.Rating)\
+            .join(local_db_dto.Rating.movie)\
+            .join(local_db_dto.Rating.user)\
+            .where(sa.and_(
+                local_db_dto.Rating.exported == False,
+                local_db_dto.Movie.exported,
+                local_db_dto.User.exported
+            ))
+        result = await session.execute(stmt)
+        return [row.Rating for row in result]
+
+
+async def get_exported_ratings():
+    engine = await create_local_db_engine()
+    async_session = sessionmaker(
+        engine, expire_on_commit=False, class_=AsyncSession
+    )
+
+    async with async_session() as session:
+        stmt = select(local_db_dto.Rating).where(local_db_dto.Rating.exported)
+        result = await session.execute(stmt)
+        return [row.Rating for row in result]
+
+
+async def set_rating_exported(rating_id):
+    engine = await create_local_db_engine()
+    async_session = sessionmaker(
+        engine, expire_on_commit=False, class_=AsyncSession
+    )
+
+    async with async_session() as session:
+        stmt = select(local_db_dto.Rating).where(local_db_dto.Rating.id == rating_id)
+        result = await session.execute(stmt)
+        rating = result.fetchone()[0]
+        rating.exported = True
+        await session.commit()
